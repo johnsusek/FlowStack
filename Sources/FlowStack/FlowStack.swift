282a85f582f1ac8b9531b12d83293290657ca040
John Susek <john@johnsolo.net>
2019-06-25 22:06:23 -0500

    Make public

--- Sources/FlowStack/FlowStack.swift
+++ Sources/FlowStack/FlowStack.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
-struct FlowStack<Content>: View where Content: View {
+public struct FlowStack<Content>: View where Content: View {
   // The number of columns we want to display
   var columns: Int
   // The total number of items in the stack
